585ea00d9d68089af9c76020e6f8d9a1a31eba47
Kaian Samuel <kaiansamuelster@gmail.com>
2023-11-26 15:04:40 -0300

    While 4 and exercise 1

---
+++ repeticao_exerc1.py
@@ -0,0 +1,6 @@
+contador = 15
+
+while contador <= 200:
+    quadrado = contador * contador
+    print('O numero ', contador, 'elevado ao quadrado é: ', quadrado)
+    contador += 1
---
+++ while4.py
@@ -0,0 +1,8 @@
+nota = 0
+continua = 's'
+
+while continua == 's':
+    print('Digite a nota: ')
+    nota = float(input())
+    print('Deseja continuar a leitura das notas? (s/n)')
+    continua = input()
